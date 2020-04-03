0a70d7faed74af76c2563b15490de048dccc1271
Jan Jablunka <jablunka@gmail.com>
2020-04-03 11:12:43 +0200

    incremental

--- app/Controllers/Homepage.php
+++ app/Controllers/Homepage.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 class Homepage extends \Katu\Controllers\Controller
 {
-	public function index($request, $response, $args)
+	public function index(\Slim\Http\Request $request, \Slim\Http\Response $response, array $args)
 	{
 		return $this->render($request, $response, $args, "Homepage/index.twig");
 	}
